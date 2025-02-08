518ebcd58a588ae63904754ba7ede0d75be87b2b
khajaSabik <khajasabik@gmail.com>
2025-02-09 00:50:32 +0600

    Added GitHub Actions workflow for Docker CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("chaning the text!");
+  res.send("Hello form new world!");
 });
 
 const PORT = process.env.PORT || 3000;
