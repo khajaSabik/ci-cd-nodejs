75deb4ca08817a337579f52baa04fef191ee8e26
anjanliberate <anjan@liberate-labs.com>
2025-07-02 13:49:07 +0600

    modify app.js port 3000 to 4000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Dhaka world!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
